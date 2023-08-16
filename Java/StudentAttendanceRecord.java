547000779f7343e6f51870bd7ada66377b6eb51b
koboko34 <66181314+koboko34@users.noreply.github.com>
2023-08-16 21:07:54 +0100

    Solved 1 java problem

---
+++ Java/StudentAttendanceRecord.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class StudentAttendanceRecord {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static boolean checkRecord(String s) {
+        int absentCount = 0;
+        int lateCount = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+        	switch (s.charAt(i)) {
+        		case 'P':
+        			lateCount = 0;
+        			continue;
+        		case 'L':
+        			if (lateCount == 2)
+        				return false;
+        			lateCount++;
+        			continue;
+        		case 'A':
+        			if (absentCount == 1)
+        				return false;
+        			absentCount++;
+        			lateCount = 0;
+        			continue;
+        	}
+        }
+		
+		return true;
+    }
+}
