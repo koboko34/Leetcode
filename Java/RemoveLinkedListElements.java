ab15e66b9dc1c8850d9590e694fb70174dc5f5e7
koboko34 <66181314+koboko34@users.noreply.github.com>
2023-09-09 07:00:54 +0900

    Solved 1 java problem

---
+++ Java/RemoveLinkedListElements.java
@@ -0,0 +1,43 @@
+
+public class RemoveLinkedListElements {
+
+	public static void main(String[] args) {
+		ListNode nine = new ListNode(9);
+		ListNode eight = new ListNode(3, nine);
+		ListNode seven = new ListNode(7, eight);
+		ListNode six = new ListNode(3, seven);
+		ListNode five = new ListNode(5, six);
+		ListNode four = new ListNode(4, five);
+		ListNode three = new ListNode(3, four);
+		ListNode two = new ListNode(1, three);
+		ListNode one = new ListNode(1, two);
+		
+		removeElements(one, 3);
+	}
+	
+	public static ListNode removeElements(ListNode head, int val) {
+		// time complexity: O(n)
+		// space complexity: O(1)
+		
+		if (head == null)
+			return null;
+		
+		while (head.val == val)
+			head = head.next;
+		
+		ListNode prev = head;
+		ListNode cur = head.next;
+		while (cur != null) {
+			if (cur.val == val) {
+				cur = cur.next;
+				prev.next = cur;
+				continue;
+			}
+			prev = cur;
+			cur = cur.next;
+		}
+		
+		return head;
+	}
+
+}
